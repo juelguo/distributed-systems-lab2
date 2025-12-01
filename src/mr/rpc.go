e81e0f3f556d9a0ced5956d246438e700e85418b
jas0n2k <jas0n2k@outlook.com>
2025-12-01 02:25:01 +0000

    feat: add distributed mode in RPC module

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -68,8 +68,27 @@ type TaskDoneReply struct {
 // in /var/tmp, for the coordinator.
 // Can't use the current directory since
 // Athena AFS doesn't support UNIX-domain sockets.
+
+/**
+ *	Author: Pengfei Li
+ *	Add distributed mode to the basic MapReduce framework.
+ *  By setting env varible "MR_COORDINATOR" to the coordinator's IP address and port,
+ *  the worker can connect to the coordinator in distributed mode.
+ *  If the env variable is not set, the worker will fall back to the original local mode."
+**/
+
 func coordinatorSock() string {
+	// for distributed mode
+	if addr := os.Getenv("MR_COORDINATOR"); addr != "" {
+		return addr
+	}
+
+	// for local mode
 	s := "/var/tmp/5840-mr-"
-	s += strconv.Itoa(os.Getuid())
-	return s
+    s += strconv.Itoa(os.Getuid())
+    return s
+}
+
+func IsDistributed() bool {
+	return os.Getenv("MR_COORDINATOR") != ""
 }
